3fe644dbfdd22dee10868a35d7f889f11ab799d0
Everton Sousa <evertonsousa951@gmail.com>
2023-01-09 03:24:52 -0300

    descobrir se um número é primo

--- CursoEmVideo/ex14.py
+++ CursoEmVideo/ex14.py
@@ -1,5 +1,13 @@
+tot = 0
 n = int(input('Digite um número: '))
-if n / 1 and n / n:
-    print('É um número primo')
+for c in range(1, n + 1):
+    if n % c == 0:
+        print(f'Divisivel por: {c}')
+        tot += 1
+    else:
+        print(f'Não é divisivel por {c}')
+print(f'O número {n} foi divisivel {tot} vezes.')
+if tot == 2:
+    print(f'{n} é um número PRIMo')
 else:
-    print('Não é')
+    print(f'{n} não é um número PRIMO.')
