6f1769e116e8c30fd82a33a7a5d9e4fb1733abc5
Everton Sousa <evertonsousa951@gmail.com>
2023-01-15 20:10:21 -0300

    Update ex28.py

--- CursoEmVideo/ex28.py
+++ CursoEmVideo/ex28.py
@@ -1,1 +1,14 @@
 #jogo de par ou impar - perguntar se quer par ou impar - contar quantas vezes você venceu - so parar quando você perder o jogo
+from random import randint
+s = 0
+while True:
+    pc = randint(0, 10)
+    print('Jogo do PAR ou ÍMPAR')
+    n = int(input('Digite um valor: '))
+    s = pc + n
+    escolha = str(input('Você quer PAR ou ÍMPAR [P/I]? ')).strip().upper()[0]
+    while escolha not in 'PI':
+        print('Valor Inválido. Tente novamente')
+        escolha = str(input('Você quer PAR ou ÍMPAR [P/I]? ')).strip().upper()[0]
+        print(escolha)
+    print('Programa Finalizado.')
