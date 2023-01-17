eea2fb088bff7880e3c13df5dda568cbb8e21287
Everton Sousa <evertonsousa951@gmail.com>
2023-01-17 15:30:45 -0300

    Update ex32.py

--- ex32.py
+++ ex32.py
@@ -1,7 +1,7 @@
 ## idade - sexo - quer continuar - quantos homens e mulheres - mulheres com mais de 18 anos - quantas pessoas com mais de 18 anos
 from datetime import date
 ano = date.today().year
-totHom = totMul = MulherMaior = totMaiores = 0
+totHom = totMul = MulherMaior = HomemMaior = totMaiores = 0
 while True:
     nasc = int(input('Ano de nascimento: '))
     idade = ano - nasc
@@ -15,4 +15,14 @@ while True:
         escolha = str(input('Quer continuar ? [S/N]: ')).strip().upper()[0]
     if escolha == 'N':
         break
-    
+    if sexo == 'M':
+        totHom += 1
+    else:
+        totMul += 1
+    if sexo == 'F' and idade >= 18:
+        MulherMaior += 1
+    if sexo == 'M' and idade >= 18:
+        HomemMaior += 1
+s = MulherMaior + HomemMaior
+print(f'Tem {totHom} homem(ns) e {totMul} mulher(es).')
+print(f'Tem {MulherMaior} mulher(es) maior de idade e {HomemMaior} homens maiores de idade, totalizando {s} pessoa(s) maiores de idade.')
