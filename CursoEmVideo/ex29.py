9672911e4c796ecb5017dba0454c0d8dcea71779
Everton Sousa <evertonsousa951@gmail.com>
2023-01-16 21:10:38 -0300

    Update ex29.py

--- CursoEmVideo/ex29.py
+++ CursoEmVideo/ex29.py
@@ -1,4 +1,14 @@
-from math import factorial
+'''from math import factorial
 n = int(input('Digite o fatorial que você quer: '))
 f = factorial(n)
+print(f)'''
+
+n = int(input('Digite o Fatorial que você quer: '))
+c = n
+f = 1
+while c > 0:
+    print('{}'.format(c), end=' ')
+    print(' x ' if c > 1 else ' = ', end=' ')
+    f *= c
+    c -= 1
 print(f)
