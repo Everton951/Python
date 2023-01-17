d5267be085bbdc2b8ba1a79466636c2e47aed62a
Everton Sousa <evertonsousa951@gmail.com>
2023-01-17 15:13:13 -0300

    Update ex32.py

--- ex32.py
+++ ex32.py
@@ -1,2 +1,10 @@
 from datetime import date
 ano = date.today().year
+while True:
+    nasc = int(input('Ano de nascimento: '))
+    idade = ano - nasc
+    sexo = str(input('Qual seu sexo ? [M/F]: ')).strip().upper()[0]
+    while sexo not in 'M F':
+        print('Valor Inválido. Tente novamente.')
+        sexo = str(input('Qual seu sexo ? [M/F]: ')).strip().upper()[0]
+    
