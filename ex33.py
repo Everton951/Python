243fccaa3a646d304cef4dc5591c1b32571e0d3d
Everton Sousa <evertonsousa951@gmail.com>
2023-01-19 16:41:37 -0300

    Update ex33.py

--- ex33.py
+++ ex33.py
@@ -1,6 +1,6 @@
-# ler o nome do produto e seu preço - perguntar se quer continuar ou não - total da compra - quantos curstam mais de 1000 - nome do produto mais barato
+# ler o nome do produto e seu preço - perguntar se quer continuar ou não - total da compra - quantos curstam mais de 1000 - nome do produto mais barato - nome do produto mais caro
 escolha = nome = ''
-total = Maiosde1000 = ProdutoBarato = 0
+total = Maiosde1000 = ProdutoBarato = Produtocaro = 0
 cont = 1
 while escolha != 'N':
     produto = str(input('Qual o nome do produto: ')).strip()
@@ -18,10 +18,18 @@ while escolha != 'N':
         if preco < ProdutoBarato:
             ProdutoBarato = preco
             nome = produto
+    if cont == 1:
+        Produtocaro = preco
+        nome = produto
+    else:
+        if preco > Produtocaro:
+            Produtocaro = preco
+            nome = produto
     cont += 1
 print('-' * 30)
 if Maiosde1000 >= 1:        
     print(f'Tem {Maiosde1000} produto(s) acima de 1000 reais.')
 else:
     print(f'Não há nenhum produto acima de 1000 reais.')
-print(f'O produto mais barato é o {nome.capitalize()} e custa {ProdutoBarato :.2f}')
+print(f'O produto mais barato é o {nome.capitalize()} e custa {ProdutoBarato :.2f} reais.')
+print(f'O produto mais caro é o {nome.capitalize()} e custa {Produtocaro :.2f} reais.')
