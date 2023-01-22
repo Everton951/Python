2ab5af2bcde919d6dab23764182f7a8725b9dfbc
Everton Sousa <evertonsousa951@gmail.com>
2023-01-22 17:05:46 -0300

    Update ex36.py

--- ex36.py
+++ ex36.py
@@ -2,4 +2,6 @@
 cont = ['zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'novo', 'dez']
 while True:
     n = int(input('Digite um número entre 0 e 10: '))
-    
+    if n <= 10 and n >= 0:
+        break
+    print(' Tente novamente.', end='')
