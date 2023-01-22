abde70ac181f6f5db24447c65f616459c4e002d2
Everton Sousa <evertonsousa951@gmail.com>
2023-01-22 17:02:22 -0300

    Update ex36.py

--- ex36.py
+++ ex36.py
@@ -1,1 +1,5 @@
 # NÚMERO POR EXTENSO - DIGITAR UM NÚMERO (INTEIRO) - E FAZER O PROGRAMA DIZER POR EXTENSO
+cont = ['zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'novo', 'dez']
+while True:
+    n = int(input('Digite um número entre 0 e 10: '))
+    
