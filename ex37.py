5c64ecd4d0b39eb68026720da53880a6a6337429
Everton Sousa <evertonsousa951@gmail.com>
2023-01-22 19:05:12 -0300

    Update ex37.py

--- ex37.py
+++ ex37.py
@@ -1,2 +1,6 @@
 # CRIE UMA TUPLA COM OS TIMES DO BRASILEIRÃO - OS 5 PRIMEIROS - OS ÚLTIMOS 4 COLOCADOS - TIMES EM ORDEM ALFABÉTICA - EM QUE POSIÇÃO ESTÁ O TIME DO CUIABÁ
-times = ['Flamengo', 'Palmeiras', 'Internacional', 'Fluminense', 'COrinthians', 'Athletico-PR', 'Atlético-MG', 'Fortaleza', 'São Paulo', 'América-MG', 'Botafogo', 'Santos', 'Goiás', 'Bragantino', 'Coritiba', 'Cuiabá', 'Ceará', 'Atlético-GO', 'Avaí', 'Juventude']
+from time import sleep
+times = ['Flamengo', 'Palmeiras', 'Internacional', 'Fluminense', 'Corinthians', 'Athletico-PR', 'Atlético-MG', 'Fortaleza', 'São Paulo', 'América-MG', 'Botafogo', 'Santos', 'Goiás', 'Bragantino', 'Coritiba', 'Cuiabá', 'Ceará', 'Atlético-GO', 'Avaí', 'Juventude']
+for t in times:
+    print(t)
+    sleep(1)
