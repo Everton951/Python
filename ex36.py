02ebad23154927a59c794cfa9d88db822af52c22
Everton Sousa <evertonsousa951@gmail.com>
2023-01-22 17:15:05 -0300

    pegar números por extenso

--- ex36.py
+++ ex36.py
@@ -1,8 +1,14 @@
 # NÚMERO POR EXTENSO - DIGITAR UM NÚMERO (INTEIRO) - E FAZER O PROGRAMA DIZER POR EXTENSO
-cont = ['zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'novo', 'dez']
+cont = ['zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez']
 while True:
-    n = int(input('Digite um número entre 0 e 10: '))
-    if n <= 10 and n >= 0:
+    while True:
+        n = int(input('Digite um número entre 0 e 10: '))
+        if n <= 10 and n >= 0:
+            break
+        print('Tente novamente.', end=' ')
+    print(f'Você digitou o número {cont[n]}')
+    escolha = str(input('Você quer continuar ? [S/N]: ')).strip().upper()[0]
+    if escolha == 'N':
         break
-    print('Tente novamente.', end=' ')
-print(f'Você digitou o número {cont[n]}')
+print('-' * 30)
+print('Programa Finalizado!')
