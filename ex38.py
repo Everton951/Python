c6b8b5af19735c0c3bddf9c4754d60653eb3b16a
Everton Sousa <evertonsousa951@gmail.com>
2023-01-23 09:33:09 -0300

    Update ex38.py

--- ex38.py
+++ ex38.py
@@ -1,4 +1,4 @@
 # GERAR 5 NÚMEROS ALEATÓRIOS E COLOCAR NUMA TUPLA - MOSTRAR TODOS OS VALORES - MOSTRAR MAIOR E O MENOR VALOR
 from random import randint
-n = randint(0, 5)
+n = [randint(0, 5), randint(0, 5), randint(0, 5), randint(0, 5), randint(0, 5)]
 print(n)
