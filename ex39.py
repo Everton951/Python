0b62ac26466bffe1d6cbe35605a321b3af2e0bd1
Everton Sousa <evertonsousa951@gmail.com>
2023-01-23 20:36:59 -0300

    Update ex39.py

--- ex39.py
+++ ex39.py
@@ -1,1 +1,3 @@
 # LER 4 VALORES E GUARDAR NUMA TUPLA - QUANTAS VEZES APARECE O VALOR 9 - POSIÇÃO DO PRIMEIRO VALOR 3 - QUAIS SÃO OS NÚMEROS PARES
+n = [int(input('Digite um número: ')), int(input('Digite outro número: ')),
+    int(input('Digite mais um número: ')), int(input('Digite o último número: '))]
