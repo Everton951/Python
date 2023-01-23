00911fd7b7642cc36483e9d734cb06c607d0cda2
Everton Sousa <evertonsousa951@gmail.com>
2023-01-23 20:43:31 -0300

    Update ex39.py

--- ex39.py
+++ ex39.py
@@ -4,3 +4,4 @@ n = [int(input('Digite um número: ')), int(input('Digite outro número: ')),
     int(input('Digite o último número: '))]
 print(f'Você digitou os valores: {n}')
 print(f'O valor 9 apareceu {n.count(9)} vezes.')
+print(f'O valor 3 apareceu na posição {n.index(3) + 1}')
