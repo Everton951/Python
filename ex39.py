b3700d8ac15ba67bfcfb74f36f88f5a722937cae
Everton Sousa <evertonsousa951@gmail.com>
2023-01-23 20:50:38 -0300

    Update ex39.py

--- ex39.py
+++ ex39.py
@@ -4,7 +4,10 @@ n = [int(input('Digite um número: ')), int(input('Digite outro número: ')),
     int(input('Digite o último número: '))]
 print(f'Você digitou os valores: {n}')
 print(f'O valor 9 apareceu {n.count(9)} vezes.')
-print(f'O valor 3 apareceu na posição {n.index(3) + 1}')
+if 3 in n:
+    print(f'O valor 3 apareceu na posição {n.index(3) + 1}')
+else:
+    print('O valor 3 não foi digitado.')
 print(f'Os valores pares digitados foram:', end=' ')
 for c in n:
     if c % 2 == 0:
