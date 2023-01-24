f001271dd861392ff0169b794ff1ca6379af7835
Everton Sousa <evertonsousa951@gmail.com>
2023-01-24 20:16:22 -0300

    Update ex40.py

--- ex40.py
+++ ex40.py
@@ -1,7 +1,10 @@
 # LISTA DE PREÇOS COM TUPLAS
 listagem = ['Lápis', 1.75, 'Borracha', 2, 'Caderno', 15.90, 'Estojo', 25, 'Transferidor', 4.20, 'Compasso', 9.90, 'Mochila', 120.30, 'Canetas', 22.30, 'Livro', 34.90]
+print('-' * 35)
+print('LISTAGEM DE PREÇOS')
+print('-' * 35)
 for contador in range(0, len(listagem)):
     if contador % 2 == 0:
         print(f'{listagem[contador]:.<30}', end='')
     else:
-        print(f'{listagem[contador]:>7}')
+        print(f'{listagem[contador]:>7.2f}')
