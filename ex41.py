2b5a68352e78d11e91bc706f43a43fbeada0e518
Everton Sousa <evertonsousa951@gmail.com>
2023-01-24 20:49:39 -0300

    Update ex41.py

--- ex41.py
+++ ex41.py
@@ -1,1 +1,8 @@
-print()
+# CRIAR UMA TUPLA COM VÁRIAS PALAVRAS (SEM ACENTOS) - MOSTRAR AS VOGAIS
+palavras = ['aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro']
+for c in palavras:
+    print(f'\nNa palavra {c.upper()} temos ', end='')
+    for letra in c:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
+print('\nPrograma Finalizado !')
