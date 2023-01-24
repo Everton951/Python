0491c2a2902c058fe7be41d135efc77f4df5503d
Everton Sousa <evertonsousa951@gmail.com>
2023-01-24 20:03:57 -0300

    Update ex40.py

--- ex40.py
+++ ex40.py
@@ -1,2 +1,3 @@
 # LISTA DE PREÇOS COM TUPLAS
-print()
+listagem = ['Lápis', 1.75, 'Borracha', 2, 'Caderno', 15.90, 'Estojo', 25, 'Transferidor', 4.20, 'Compasso', 9.90, 'Mochila', 120.30, 'Canetas', 22.30, 'Livro', 34.90]
+print(listagem)
