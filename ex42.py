2a4a02198d7d4dfe73cc4280c50b9cea9040305b
Everton Sousa <evertonsousa951@gmail.com>
2023-01-25 20:46:32 -0300

    Update ex42.py

--- ex42.py
+++ ex42.py
@@ -1,6 +1,7 @@
 n = [1, 2, 3, 4, 5] 
 del n[2] #ELIMINAR O NÚMERO 3
 n.pop[3] #ELIMINAR O NÚMERO 3
+n.pop() #ELIMINA O ÚLTIMO VALOR DA LISTA
 n.remove(3) #ELIMINAR O NÚMERO 3
 n[3] = 10
 for c in n:
