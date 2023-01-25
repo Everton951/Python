709366a80cdcd8683d067ee52802936d49f8d5fb
Everton Sousa <evertonsousa951@gmail.com>
2023-01-25 20:58:41 -0300

    Update ex42.py

--- ex42.py
+++ ex42.py
@@ -1,7 +1,12 @@
-n = [1, 2, 3, 4, 5] 
+n = [1, 2, 3, 4, 5]
+n.append(7) # ADICIONAR O VALOR 7
 del n[2] #ELIMINAR O NÚMERO 3
-n.pop[3] #ELIMINAR O NÚMERO 3
-n.pop() #ELIMINA O ÚLTIMO VALOR DA LISTA
-n.remove(3) #ELIMINAR O NÚMERO 3
+#n.pop[3] #ELIMINAR O NÚMERO 3
+#n.pop() #ELIMINA O ÚLTIMO VALOR DA LISTA
+#n.remove(3) #ELIMINAR O NÚMERO 3
 n[3] = 10
+n.insert(2, 0) # INSERIR NA POSIÇÃO 2 O VALOR 0
 valores = list(range(0, 11)) # CRIA UMA LISTA COM OS VALORES DE 0 ATÉ 10
+for c in n:
+    print(c, end='  ')
+print()
