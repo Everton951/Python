f2adf52a0077acd770568f4080adae54b6dabbc2
Everton Sousa <evertonsousa951@gmail.com>
2023-01-26 19:52:57 -0300

    Update ex43.1.py

--- CursoEmVideo/ex43.1.py
+++ CursoEmVideo/ex43.1.py
@@ -1,4 +1,15 @@
 n = []
+maior = menor = 0
 for c in range(0, 5):
-    print(f'c: {c}')
     n.append(int(input(f'Digite um valor para a posição {c}: ')))
+    if c == 0:
+        maior = menor = n[c]
+    else:
+        if n[c] > maior:
+            maior = n[c]
+        if n[c] < menor:
+            menor = n[c]
+print('-=' * 30)
+print(f'Maior valor {maior} nas posições ', end=' ')
+
+print(f'Menor valor {menor} nas posições ', end=' ')
