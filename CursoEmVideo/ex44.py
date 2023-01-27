91e2fc0d43220b8f29c310af9dae56bd4e54ece9
Everton Sousa <evertonsousa951@gmail.com>
2023-01-27 19:56:14 -0300

    Update ex44.py

--- CursoEmVideo/ex44.py
+++ CursoEmVideo/ex44.py
@@ -5,11 +5,13 @@ for c in range(0, 5):
         lista.append(n)
     elif n > lista[-1]:
         lista.append(n)
+        print('Adicionado ao final da lista...')
     else:
         pos = 0
         while pos < len(lista):
             if n <= lista[pos]:
                 lista.insert(pos, n)
+                print(f'Adicionado na posição {pos} da lista...')
                 break
             pos += 1
 print('-=' * 30)
