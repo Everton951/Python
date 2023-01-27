62b56c52e4a165934925e2c3ecfa904614fc12a8
Everton Sousa <evertonsousa951@gmail.com>
2023-01-27 19:57:10 -0300

    Update ex44.py

--- CursoEmVideo/ex44.py
+++ CursoEmVideo/ex44.py
@@ -3,6 +3,7 @@ for c in range(0, 5):
     n = int(input('Digite um valor: '))
     if c == 0:
         lista.append(n)
+        print('Adicionado ao final da lista...')
     elif n > lista[-1]:
         lista.append(n)
         print('Adicionado ao final da lista...')
