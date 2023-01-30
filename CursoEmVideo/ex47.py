2e5f1eaf18d20b1b1e54a1c5923cf714b36488d7
Everton Sousa <evertonsousa951@gmail.com>
2023-01-29 22:48:40 -0300

    Update ex47.py

--- CursoEmVideo/ex47.py
+++ CursoEmVideo/ex47.py
@@ -6,3 +6,10 @@ for simbolo in expr:
     elif simbolo == ')':
         if len(pilha) > 0:
             pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')
