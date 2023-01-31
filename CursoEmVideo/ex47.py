b9dfb935a429de64b9f637fd6f73bb7023c42f16
Everton Sousa <evertonsousa951@gmail.com>
2023-01-30 22:28:00 -0300

    Update ex47.py

--- CursoEmVideo/ex47.py
+++ CursoEmVideo/ex47.py
@@ -14,27 +14,3 @@ if len(pilha) == 0:
 else:
     print('Sua expressão está errada!')
 
-
-package relembrando;
-
-import java.util.Calendar;
-import java.util.Scanner;
-
-public class Relembrando {        
-
-    public static void main(String[] args) { 
-        
-        
-        int n[] = {3, 2, 4, 5, 7, 6, 8};
-        
-        for(int i = 0; i < n.length; i++) {
-            System.out.print(n[i] + "  ");
-        }
-        System.out.println("\nTotal de números: " + n.length);
-        
-        for(int a = 0; a < n.length; a++) {
-            System.out.println("Na posição " + (a + 1) + " temos o valor " + n[a]);
-        }
-    }
-    
-}
